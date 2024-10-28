2bbe6209d93368419e767ab13d9d097a2a71828b
rmvalentim <rmoraesvalentim@gmail.com>
2024-10-28 17:04:18 -0300

    User routes implemented

---
+++ src/main/java/br/com/rafaelvalentim/Sales/controllers/UserController.java
@@ -0,0 +1,36 @@
+package br.com.rafaelvalentim.Sales.controllers;
+
+import br.com.rafaelvalentim.Sales.entities.User;
+import br.com.rafaelvalentim.Sales.services.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequestMapping("/users")
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<User> authenticatesUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User currentUser = (User) authentication.getPrincipal();
+        return ResponseEntity.ok(currentUser);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<User>> allUsers() {
+        List<User> users = userService.allUsers();
+        return ResponseEntity.ok(users);
+    }
+}
